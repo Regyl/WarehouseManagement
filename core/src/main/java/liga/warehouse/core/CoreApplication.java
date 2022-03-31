baacb454a236908c96eaa305fc1e1f19a9c08c8c
Regyl <novikovevgeny1@gmail.com>
2022-03-31 19:52:28 +0300

    finish modules

--- core/src/main/java/liga/warehouse/core/CoreApplication.java
+++ core/src/main/java/liga/warehouse/core/CoreApplication.java
@@ -1,13 +1,13 @@
-package liga.warehouse.app;
+package liga.warehouse.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WarehouseApplication {
+public class CoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(WarehouseApplication.class, args);
+        SpringApplication.run(CoreApplication.class, args);
     }
 
 }
