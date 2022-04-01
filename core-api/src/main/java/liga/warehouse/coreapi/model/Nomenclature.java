0b252f0a296e6a81e9acb69608a9a24f22ccce71
Regyl <novikovevgeny1@gmail.com>
2022-04-01 07:38:13 +0300

    finish nomenclature

--- core-api/src/main/java/liga/warehouse/coreapi/model/Nomenclature.java
+++ core-api/src/main/java/liga/warehouse/coreapi/model/Nomenclature.java
@@ -1,15 +1,24 @@
 package liga.warehouse.coreapi.model;
 
+import liga.warehouse.coreapi.model.enumeration.FrequencyType;
 import liga.warehouse.coreapi.model.enumeration.ProductType;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
-import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class Nomenclature extends AbstractEntity {
 
     private String name;
 
-    private ProductType type;
+    private ProductType productType;
+
+    private FrequencyType frequencyType;
+
+    private String country;
+
+    private LocalDateTime createTimestamp;
 
 }
---
+++ core-api/src/main/java/liga/warehouse/coreapi/model/enumeration/FrequencyType.java
@@ -0,0 +1,7 @@
+package liga.warehouse.coreapi.model.enumeration;
+
+public enum FrequencyType {
+    DAILY, //повседневный спрос
+    LONG, //длительное использование
+    RARELY //импульсные покупки
+}
