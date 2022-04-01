4e137d7019c12a1b5b3f6d7da713d981ccbe9e22
Regyl <novikovevgeny1@gmail.com>
2022-04-01 20:35:40 +0300

    add changelog

---
+++ core-api/src/main/java/liga/warehouse/coreapi/model/Role.java
@@ -0,0 +1,14 @@
+package liga.warehouse.coreapi.model;
+
+import liga.warehouse.dto.enumeration.Authority;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Role extends AbstractEntity implements GrantedAuthority {
+
+    private Authority authority;
+
+}
---
+++ core-api/src/main/java/liga/warehouse/coreapi/model/User.java
@@ -0,0 +1,55 @@
+package liga.warehouse.coreapi.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Set;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class User extends AbstractEntity implements UserDetails {
+
+    private String username;
+
+    private String password;
+
+    private Set<Role> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
