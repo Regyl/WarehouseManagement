15d239cdb9ca534e5f5a785595bd52f5d74df970
Regyl <novikovevgeny1@gmail.com>
2022-04-01 21:35:34 +0300

    finish user repo

--- core/src/main/java/liga/warehouse/core/repository/UserRepository.java
+++ core/src/main/java/liga/warehouse/core/repository/UserRepository.java
@@ -1,14 +1,17 @@
 package liga.warehouse.core.repository;
 
+import liga.warehouse.coreapi.model.Role;
 import liga.warehouse.coreapi.model.User;
 import liga.warehouse.coreapi.repository.CrudRepository;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Mapper
 public interface UserRepository extends CrudRepository<User> {
 
     Optional<User> findByUsername(String username);
 
+    int saveRoles(Long memberId, Set<Role> roles);
 }
